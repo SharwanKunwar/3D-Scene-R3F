146af20eaf60ec407a1b550dff816f5d3d508690
SharwanKunwar <sharwankunwar07@gmail.com>
2025-07-07 17:21:17 +0545

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+'use client';
+
 import { Canvas } from "@react-three/fiber";
-import Myscene from '../components/Myscene'; 
+import { Geist, Geist_Mono } from "next/font/google";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+import { Html } from "@react-three/drei"; // ✅ IMPORTANT: Add this!
+
+const Myscene = dynamic(() => import("../components/Myscene"), { ssr: false });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,12 +20,20 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <>
     <div className="w-screen h-screen">
       <Canvas>
-      <Myscene/>
-    </Canvas>
+        <Suspense
+          fallback={
+            <Html center>
+              <div className="text-white text-lg bg-black  rounded flex justify-center items-center">
+                <p className="w-[300px] h-[100px] py-2 px-10">Wait a sec, it will render soon...</p>
+              </div>
+            </Html>
+          }
+        >
+          <Myscene />
+        </Suspense>
+      </Canvas>
     </div>
-    </>
   );
 }
